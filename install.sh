cb323efe232868292678ccc54185f5c8da8af483
KingKor <lulzatyourface@tuta.io>
2016-05-24 14:42:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-sudo chmod -R 755 ./king.sh
-sudo chmod -R 755 ./kingd.sh
-sudo chmod -R 755 ./kingking.sh
+sudo chmod -R 755 ./*
 cd ~
-echo 'alias king2="~//king.sh"' >> .bash_aliases
-echo 'alias tora="/usr/sbin/tor -f ~/KingKor/torrc2"' >> ~/.bash_aliases
+echo 'alias king2="~/KingKor/king.sh"' >> .bash_aliases
+echo 'alias tor1="/usr/sbin/tor -f ~/KingKor/torrc2"' >> ~/.bash_aliases
+echo 'alias tor2="/usr/sbin/tor -f ~/KingKor/torrc3"' >> ~/.bash_aliases
 source ~/.bash_aliases
