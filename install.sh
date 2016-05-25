92b85dac4610d8b60bee8ed6deeea42ad4a47fc0
KingKor <lulzatyourface@tuta.io>
2016-05-25 18:02:06 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-sudo chmod -R 755 ./*
-echo 'alias tor1="/usr/sbin/tor -f ~/KingKor/torrc2"' >> ~/.bash_aliases
-echo 'alias tor2="/usr/sbin/tor -f ~/KingKor/torrc3"' >> ~/.bash_aliases
-source ~/.bash_aliases
